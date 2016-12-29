e5af05a36061d19ea5c840f1cc5b1c10e9478e65
Rory McKinley <rorymckinley@gmail.com>
2016-12-29 16:36:31 +0200

    Complete trackingdata package

--- trackingdata/trackingdata.go
+++ trackingdata/trackingdata.go
@@ -1,6 +1,7 @@
 package trackingdata
 
 import (
+	"github.com/rorymckinley/timetracker/data"
 	"time"
 )
 
@@ -18,4 +19,29 @@ type TrackingData struct {
 
 func (td *TrackingData) LastEvent() *Event {
 	return &td.Events[len(td.Events)-1]
+}
+
+func (td *TrackingData) LastEventIsOpen() bool {
+	return td.LastEvent().EndTime.IsZero()
+}
+
+func (td *TrackingData) CloseLastEvent() {
+	td.LastEvent().EndTime = time.Now()
+}
+
+func (td *TrackingData) ToggleLast() {
+	if td.LastEventIsOpen() {
+		td.CloseLastEvent()
+	} else {
+		td.AddEvent(data.Data{StartTime: time.Now(), Description: td.LastEvent().Description})
+	}
+}
+
+func (td *TrackingData) AddEvent(data data.Data) {
+	events := append(td.Events, startEvent(data))
+	td.Events = events
+}
+
+func startEvent(data data.Data) (event Event) {
+	return Event{Description: data.Description, Category: data.Category, StartTime: data.StartTime, EndTime: data.EndTime, SubCategories: data.SubCategories}
 }
--- trackingdata/trackingdata_test.go
+++ trackingdata/trackingdata_test.go
@@ -1,12 +1,66 @@
 package trackingdata
 
 import (
+	// "github.com/davecgh/go-spew/spew"
 	"testing"
+	"time"
 )
 
 func TestLastEvent(t *testing.T) {
 	td := TrackingData{Events: []Event{Event{}, Event{}}}
 	if td.LastEvent() != &td.Events[1] {
 		t.Error("Does not return last event")
+	}
+}
+
+func TestLastEventIsOpen(t *testing.T) {
+	td := TrackingData{Events: []Event{Event{}}}
+	if !td.LastEventIsOpen() {
+		t.Error("Does not report on last event open")
+	}
+}
+
+func TestLastEventIsClosed(t *testing.T) {
+	td := TrackingData{Events: []Event{Event{EndTime: time.Now()}}}
+	if td.LastEventIsOpen() {
+		t.Error("Does not report on last event open")
+	}
+}
+
+func TestClosesLastEvent(t *testing.T) {
+	td := TrackingData{Events: []Event{Event{}}}
+	td.CloseLastEvent()
+	if td.LastEventIsOpen() {
+		t.Error("Last Event not closed")
+	}
+}
+
+func TestToggleLastLastEventOpen(t *testing.T) {
+	td := TrackingData{Events: []Event{Event{}}}
+	td.ToggleLast()
+	if td.LastEventIsOpen() {
+		t.Error("Last Event not closed by toggle")
+	}
+}
+
+func TestToggleLastLastEventClosed(t *testing.T) {
+	td := TrackingData{Events: []Event{Event{Description: "foo", EndTime: time.Now()}}}
+	closed := &td.Events[0]
+	td.ToggleLast()
+
+	if len(td.Events) != 2 {
+		t.Error("Incorrect number of events")
+	}
+	if td.LastEvent() == closed {
+		t.Error("Incorrect last event")
+	}
+	if td.LastEvent().Description != closed.Description {
+		t.Error("Last Event Description not set")
+	}
+	if !td.LastEventIsOpen() {
+		t.Error("Last Event is not open")
+	}
+	if time.Since(td.LastEvent().StartTime) > time.Duration(1)*time.Second {
+		t.Error("Start Time is incorrect")
 	}
 }
