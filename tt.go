efe56c60b017a9cad5de43f9dc562b9b996c603b
Rory McKinley <rorymckinley@gmail.com>
2016-12-29 11:00:38 +0200

    Handle command line switches

--- tt.go
+++ tt.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 )
 
+var Category = flag.String("cat", "", "Category")
+var StartTime = flag.String("start", "", "Start Time")
+var EndTime = flag.String("end", "", "End Time")
+var Subs = flag.String("subs", "", "Subcategories")
+
 type Event struct {
-	Description string    `yaml: "description"`
-	StartTime   time.Time `yaml: "startTime"`
-	EndTime     time.Time `yaml: "endTime"`
+	Description   string    `yaml: "description"`
+	StartTime     time.Time `yaml: "startTime"`
+	EndTime       time.Time `yaml: "endTime"`
+	Category      string    `yaml: category`
+	SubCategories []string
 }
 
 type TrackingData struct {
@@ -40,12 +49,16 @@ func (td *TrackingData) ToggleLast() {
 }
 
 func (td *TrackingData) AddEvent(data Data) {
-	events := append(td.Events, startEvent(data.Description))
+	events := append(td.Events, startEvent(data))
 	td.Events = events
 }
 
 type Data struct {
-	Description string
+	Description   string
+	Category      string
+	StartTime     time.Time
+	EndTime       time.Time
+	SubCategories []string
 }
 
 type ActionData struct {
@@ -79,14 +92,51 @@ func determineConfig() ActionData {
 		return ActionData{Action: "toggle-last"}
 	case len(flag.Args()) == 1:
 		data := Data{Description: flag.Arg(0)}
+		SetCategory(&data)
+		SetStartTime(&data)
+		SetEndTime(&data)
+		SetSubCategories(&data)
 		return ActionData{Action: "create-new", Data: data}
 	default:
 		return ActionData{}
 	}
 }
 
-func startEvent(description string) (event Event) {
-	return Event{Description: description, StartTime: time.Now()}
+func SetCategory(data *Data) {
+	if *Category != "" {
+		data.Category = *Category
+	} else {
+		fmt.Println("Please supply a category")
+		os.Exit(1)
+	}
+}
+
+func SetStartTime(data *Data) {
+	const dateTemplate = "2006 January 2"
+	const timeTemplate = "2006 January 2 15:04 -0700"
+	if *StartTime != "" {
+		input := time.Now().Format(dateTemplate) + " " + *StartTime + " +0200"
+		data.StartTime, _ = time.Parse(timeTemplate, input)
+	} else {
+		data.StartTime = time.Now()
+	}
+}
+
+func SetEndTime(data *Data) {
+	const dateTemplate = "2006 January 2"
+	const timeTemplate = "2006 January 2 15:04 -0700"
+	if *EndTime != "" {
+		input := time.Now().Format(dateTemplate) + " " + *EndTime + " +0200"
+		data.EndTime, _ = time.Parse(timeTemplate, input)
+	}
+}
+
+func SetSubCategories(data *Data) {
+	data.SubCategories = strings.Split(*Subs, ",")
+}
+
+func startEvent(data Data) (event Event) {
+	return Event{Description: data.Description, Category: data.Category, StartTime: data.StartTime, EndTime: data.EndTime, SubCategories: data.SubCategories}
 }
 
 // Called without args: If running - stop current running event - if stopped, create a copy of last event and start it
